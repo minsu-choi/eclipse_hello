e433983ff9636fe772f9263e65762573dfb135ae
minsu choi <choi43212@naver.com>
2017-06-30 09:04:29 +0900

    hotfix branch change a code

--- javatest/src/git_test/HelloWorld.java
+++ javatest/src/git_test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("World's End Dancehall");
+		System.out.println("Hello Dance world");
 	}
 }
