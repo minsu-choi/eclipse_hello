40f7aceea65b19741b6e44536e8a85529f5c29df
minsu choi <choi43212@naver.com>
2017-07-06 09:13:33 +0900

    modified local repository

--- javatest/src/git_test/PushPrint.java
+++ javatest/src/git_test/PushPrint.java
@@ -3,5 +3,6 @@ package git_test;
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Add Java Class File Push");
+		System.out.println("World with Miku");
 	}
 }
